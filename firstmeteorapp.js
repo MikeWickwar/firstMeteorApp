ae1e8dd1c3123f6c3d538260459e7b01cec19a19
Mike Wickwar <mike.wickwar@gmail.com>
2016-03-13 17:11:04 -0600

    stylus installed, user login installed, meteor is pure magic

--- firstmeteorapp.js
+++ firstmeteorapp.js
@@ -3,7 +3,14 @@ Resolutions = new Mongo.Collection('resolutions');
 if (Meteor.isClient) {
   Template.body.helpers({
     resolutions: function () {
+      if(Session.get('hideFinished')){
+        return Resolutions.find({checked: {$ne: true}});
+      } else {
       return Resolutions.find();
+      }
+    },
+    hideFinished: function () {
+      return Session.get('hideFinished');
     }
   });
 
@@ -19,8 +26,25 @@ if (Meteor.isClient) {
       event.target.title.value = "";
 
       return false;
+    },
+    'change .hide-finished': function (event) {
+      Session.set('hideFinished', event.target.checked)
     }
-  })
+  });
+
+  Template.resolution.events({
+    'click .toggle-checked': function () {
+      Resolutions.update(this._id, {$set: {checked: !this.checked}});
+    },
+    'click .delete': function() {
+      Resolutions.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
 
 if (Meteor.isServer) {
